866085d3ca57968258cf5cd9b46c6688d8ded072
jadsoncr <jadsoncr@gmail.com>
2026-04-20 15:16:17 -0300

    fix: use standard PrismaClient v5, restore schema url, remove Neon adapter

--- src/infra/db.js
+++ src/infra/db.js
@@ -1,22 +1,11 @@
 // src/infra/db.js
 const { PrismaClient } = require('@prisma/client');
-const { PrismaNeon } = require('@prisma/adapter-neon');
-const { Pool } = require('@neondatabase/serverless');
 
 let _prisma;
 
 function getPrisma() {
   if (!_prisma) {
-    // Use the Neon adapter which works with both regular postgres and prisma+postgres URLs
-    const adapter = new PrismaNeon(
-      new Pool({
-        connectionString: process.env.DATABASE_URL,
-      })
-    );
-
-    _prisma = new PrismaClient({
-      adapter,
-    });
+    _prisma = new PrismaClient();
   }
   return _prisma;
 }
